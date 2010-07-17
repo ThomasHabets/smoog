441c69d112fa36bb8e9bcaf8e09267fa28b9959c
Thomas Habets <thomas@habets.pp.se>
2010-07-17 22:56:00 +0200

    show progress as we go along

--- kwRename.py
+++ kwRename.py
@@ -17,6 +17,7 @@ import re
 import sys
 
 really = True
+verbose = 1
 
 def kwRename(match, to):
     """kwRename(match, to)
@@ -26,15 +27,34 @@ def kwRename(match, to):
     If 'to' is empty, just remove all keywords matching 'match'.
     """
     s = smug.Smug(*smug.userCredentials())
+    n = 0
+    n2 = 0
+    albumn = 0
+    changedn = 0
     for album in s.getAlbums():
+        albumn += 1
         for image in album.getImages():
+            n += 1
+            n2 += 1
             old = image.getKeywords()
             new = []
             for k in old:
                 k = re.sub(match, to, k)
                 if len(k) and (not k in new):
                     new.append(k)
+            if verbose:
+                s = "Album: %d,  Image %d (%d total),  Changed: %d" %(albumn,
+                                                                      n,
+                                                                      n2,
+                                                                      changedn)
+                sys.stdout.write("\r%-79s" % (s))
+                sys.stdout.write("\r%s" % (s))
+                sys.stdout.flush()
             if old != new:
+                changedn += 1
+                if verbose > 1:
+                    print dir(image)
+                    sys.exit(1)
                 if really:
                     image.changeKeywords(new)
                 else:
@@ -53,7 +73,11 @@ def usage(err):
 def main():
     if len(sys.argv) != 3:
         usage(1)
-    kwRename('^' + sys.argv[1] + '$', sys.argv[2])
+    try:
+        kwRename('^' + sys.argv[1] + '$', sys.argv[2])
+        print
+    except Exception, e:
+        print >>sys.stderr, e
     
 if __name__ == '__main__':
     main()
