b558969e49620520640e62b047a96ad1ff29c87c
Thomas Habets <thomas@habets.pp.se>
2010-07-17 22:57:10 +0200

    blaha

--- listAlbums.py
+++ listAlbums.py
@@ -4,13 +4,22 @@ import smoog as smug
 
 def html():
     s = smug.Smug(*smug.userCredentials())
-    print '<ul>'
-    for a in [x for x in s.getAlbums()
-              if x.Category['Name'] != 'fnalbum']:
+    albums = [x for x in s.getAlbums()
+              if x.Category['Name'] != 'fnalbum'
+              and x.Public == True]
+    albums.sort()
+    cat = None
+    for a in albums:
+        if cat != a.Category['Name']:
+            if cat is not None:
+                print '</ul>'
+            cat = a.Category['Name']
+            print '<h2>%s</h2><ul>' % (cat)
         print ('''<li><a href="%(url)s">%(name)s</a></li>'''
                % ({'url': a.getUrl(),
                    'name': a.Title}))
-    print '</ul>'
+    if cat is not None:
+        print '</ul>'
 
 def main():
     html()
