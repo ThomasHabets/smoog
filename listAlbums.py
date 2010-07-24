abc83ad0298bb95cd5d79872f258902521ce2f02
Thomas Habets <thomas@habets.pp.se>
2010-07-25 00:17:01 +0200

    utf-8-safe listAlbums

--- listAlbums.py
+++ listAlbums.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import smoog as smug
 
@@ -9,6 +10,18 @@ def html():
               and x.Public == True]
     albums.sort()
     cat = None
+    print '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Fnalbum</title>
+  <meta http-equiv="content-type"
+        content="text/html;charset=utf-8" />
+</head>
+<body>
+  <h1>Fnalbum</h1>
+'''
     for a in albums:
         if cat != a.Category['Name']:
             if cat is not None:
@@ -17,9 +30,10 @@ def html():
             print '<h2>%s</h2><ul>' % (cat)
         print ('''<li><a href="%(url)s">%(name)s</a></li>'''
                % ({'url': a.getUrl(),
-                   'name': a.Title}))
+                   'name': a.Title})).encode('utf-8')
     if cat is not None:
         print '</ul>'
+    print '''</body></html>'''
 
 def main():
     html()
